d059354a59d2f49bbc9156e403abbc9455ec20a1
Katrina Agustin <katrina.agustin@gmail.com>
2019-01-29 12:59:13 -0800

    data services and main components created

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  cars: Object;
+
+  constructor(private data: DataService) { }
+
+  ngOnInit() {
+    this.data.getCars().subscribe(data => {
+      this.cars = data;
+      console.log(this.cars)
+    });
+  }
+
+}
